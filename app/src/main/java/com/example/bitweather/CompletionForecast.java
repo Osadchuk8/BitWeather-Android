196a98891cd2b00ffddcbff4dea409dcb0ee4aaa
Oleksandr <oleksandr.osadchuk@gmail.com>
2019-04-14 13:11:33 -0400

    apk upload

--- app/src/main/java/com/example/bitweather/CompletionForecast.java
+++
@@ -1,8 +0,0 @@
-package com.example.bitweather;
-
-import android.location.Location;
-
-public interface CompletionForecast {
-    public void completionOk(DarkSkyForecast forecast);
-    public void completionError(String errorMessage);
-}
--- app/src/main/java/com/example/bitweather/CompletionGeoLocation.java
+++
@@ -1,8 +0,0 @@
-package com.example.bitweather;
-
-import android.location.Location;
-
-public interface CompletionGeoLocation {
-    public void completionGeocoderOk(Location location);
-    public void completionGeocoderError(String error);
-}
